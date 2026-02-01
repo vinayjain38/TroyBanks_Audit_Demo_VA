4c94546f83fb537162baa69fa442dd3bfdf4dd29
HARSHAL SANJIV PATIL <harshal.sanjivpatil2000@gmail.com>
2026-01-31 22:04:01 -0500

    file path and folder updates

--- src/Billing Engine/app_new.py
+++ src/Billing Engine/app_new.py
@@ -48,7 +48,7 @@ import sys
 from datetime import datetime
 import pandas as pd
 import numpy as np
-from src.paths import RIDERS_OUT, SCHEDULES_XLSX, EXPORT_DIR, RIDERS_OUT, USAGE_INT
+from src.Configuration_and_paths.paths import RIDERS_OUT, SCHEDULES_XLSX, EXPORT_DIR, USAGE_INT
 
 # ==== CONFIGURATION ====
 # This is va_step1_base usage file
